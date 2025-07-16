cf65504ab655ec3fdc4e6457bb38fafa8ed23b8d
k13 <mihome013@gmail.com>
2025-07-17 01:21:21 +0330

    impliment the movement

--- maze.py
+++ maze.py
@@ -1,8 +1,9 @@
 import os
 import random
 from enum import Enum
-from types import new_class, prepare_class
-from typing import Self
+from getkey import getkey
+
+keys = ["w", "s", "a", "d"]
 
 
 class objects_in_game(Enum):
@@ -15,7 +16,6 @@ class maze:
     def __init__(self, game_size) -> None:
         # game_size + walls
         self.play_ground_size = game_size + 2
-        self.keys = ["w", "s", "a", "d"]
         self.goal_location = (1, 1)
         self.player_location = (game_size, game_size)
         self.play_ground = {}
@@ -44,6 +44,8 @@ class maze:
                     pattern_row.append("#")
                 elif pos == self.goal_location:
                     pattern_row.append("G")
+                elif pos == self.player_location:
+                    pattern_row.append("P")
                 else:
                     pattern_row.append(" ")
             pattern.append(pattern_row)
@@ -108,37 +110,26 @@ class maze:
             return False
 
 
-""" my own method is not worked 
-    def _cerve_the_path(self, init_x, init_y): # In here, I try to randomly create a path instead of choosing walls.
-        # Also, to avoid looping around, I am choosing the next 2 or 3 positions.
-        # For example, if I am at (0, 0), I can choose any house within 2 or 3 (not decided yet) directions around it.
-        # Then simply just carve one position, and then in that new direction, I can decide again.
-
-        jump_size = 2
-        self.play_ground[(init_x, init_y)] = objects_in_game.space
-
-        # Choose a direction to move
-        decision = random.choice(
-            [(0, jump_size), (0, -jump_size), (jump_size, 0), (-jump_size, 0)]
-        )
-
-        next_x = init_x + decision[0]
-        next_y = init_y + decision[1]
-
-        # Check if new position is within bounds
-        if 0 < next_x < self.wall_range and 0 < next_y < self.wall_range:
-            if self.play_ground.get((next_x, next_y)) != objects_in_game.goal:
-                # if the current position is (0,0) and new direction is (0,2) , then i should cerve the position between them, which is (0,1)
-                mid_x = (init_x + next_x) // 2
-                mid_y = (init_y + next_y) // 2
-                self.play_ground[(mid_x, mid_y)] = objects_in_game.space
-
-                self._cerve_the_path(next_x, next_y)
-"""
-
-
 game = maze(16)
 game.generate_playGround()
 game.add_extra_paths(extra_count=15)
-maze_pattern = game.generate_playGround_pattern()
-game.print_maze(maze_pattern)
+while True:
+    maze_pattern = game.generate_playGround_pattern()
+    game.print_maze(maze_pattern)
+    key = getkey()
+    if key.lower() == keys[0]:
+        game_over = game.move_player(0, -1)
+    elif key.lower() == keys[1]:
+        game_over = game.move_player(0, 1)
+    elif key.lower() == keys[2]:
+        game_over = game.move_player(1, 0)
+    elif key.lower() == keys[3]:
+        game_over = game.move_player(-1, 0)
+    elif key.lower() == "q":
+        print("\nGame quit!")
+        break
+    else:
+        continue
+
+    if game_over:
+        break
