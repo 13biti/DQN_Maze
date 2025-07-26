d299d751653e75a8bb531ed8ebc6839030e06592
k13 <mihome013@gmail.com>
2025-07-26 23:23:58 +0330

    fix small bugs in both reward and epsilon policy

--- agent.py
+++ agent.py
@@ -89,6 +89,114 @@ class QNetwork:
         return history.history["loss"][0]
 
 
+# we learnd that agent should balance between exploration and exploitation
+# without this section , it seems that agent just try to explor new path
+#
+class EpsilonPolicy:
+    def __init__(
+        self,
+        epsilon_min: float = 0.01,
+        epsilon_decay: float = 0.995,
+        policy: EpsilonPolicyType = EpsilonPolicyType.DECAY,
+        update_per_episod: bool = True,
+    ):
+        self.policy = policy
+        self.visited_states = {}
+        self.epsilon: float = INFINIT
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+        self.update_per_episod = update_per_episod
+        self.old_episod = 0
+
+    def updateEpsilon(
+        self,
+        epsilon,
+        episode_count,
+        max_episodes=100,
+    ):
+        self.epsilon = epsilon
+        if self.update_per_episod:
+            if self.old_episod == episode_count:
+                return self.epsilon
+            else:
+                self.old_episod = episode_count
+        if self.policy == EpsilonPolicyType.DECAY:
+            return self.updateEpsilon_linear(episode_count, max_episodes)
+        elif self.policy == EpsilonPolicyType.SOFTLINEAR:
+            return self.updateEpsilon_SoftLinear(episode_count, max_episodes)
+        else:
+            return self.epsilon
+
+    def updateEpsilon_linear(self, episode_count, max_episodes):
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+        self.epsilon = max(self.epsilon, self.epsilon_min)
+        return self.epsilon
+
+    def updateEpsilon_SoftLinear(self, episode_count, max_episodes=200):
+        if self.epsilon > self.epsilon_min:
+            target_epsilon = max(
+                self.epsilon_min,
+                1.0 - (1.0 - self.epsilon_min) * (episode_count / max_episodes),
+            )
+            self.epsilon = self.epsilon * self.epsilon_decay + target_epsilon * (
+                1 - self.epsilon_decay
+            )
+        return self.epsilon
+
+
+class RewardHelper:
+    def __init__(
+        self,
+        progress_bonus: float = 0.05,
+        exploration_bonus: float = 0.1,
+        policy: RewardPolicyType = RewardPolicyType.ERM,
+    ):
+        self.progress_bonus = progress_bonus
+        self.exploration_bonus = exploration_bonus
+        self.policy = policy
+        self.old_huristic = INFINIT
+        self.visited_states = {}
+
+    def findReward(self, state, reward, heuristic=None, lowerHuristicBetter=True):
+        if self.policy == RewardPolicyType.ERM:
+            return self._ERM(state, reward, heuristic, lowerHuristicBetter)
+        elif self.policy == RewardPolicyType.NONE:
+            return self._none(reward)
+        else:
+            return reward
+
+    def _none(self, reward):
+        return reward
+
+    def _ERM(self, state, reward, heuristic=None, lowerHuristicBetter=True):
+        state_key = tuple(state.flatten()) if state is not None else None
+        is_new_state = state_key is not None and state_key not in self.visited_states
+        if is_new_state:
+            if is_new_state and state_key is not None:
+                self.visited_states[state_key] = (
+                    heuristic if heuristic is not None else INFINIT
+                )
+
+        progress = 0.0
+        if heuristic is not None and state_key is not None:
+            old_heuristic = self.visited_states[state_key]
+            if (
+                old_heuristic != INFINIT
+                and (heuristic < old_heuristic and lowerHuristicBetter)
+                or (heuristic > old_heuristic and not lowerHuristicBetter)
+            ):
+                progress = self.progress_bonus
+                self.visited_states[state_key] = heuristic
+
+        new_reward = reward
+        if is_new_state:
+            new_reward += self.exploration_bonus
+        if progress > 0:
+            new_reward += self.progress_bonus
+        return new_reward
+
+
 class General_DQN_Agent:
     def __init__(
         self,
@@ -166,109 +274,3 @@ class General_DQN_Agent:
         else:
             q_value = self.model.predict(current_state, verbose=0)[0]
             return np.argmax(q_value)
-
-
-# we learnd that agent should balance between exploration and exploitation
-# without this section , it seems that agent just try to explor new path
-#
-class EpsilonPolicy:
-    def __init__(
-        self,
-        epsilon_min: float = 0.01,
-        epsilon_decay: float = 0.995,
-        policy: EpsilonPolicyType = EpsilonPolicyType.DECAY,
-        update_per_episod: bool = True,
-    ):
-        self.policy = policy
-        self.visited_states = {}
-        self.epsilon: float = INFINIT
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_decay
-        self.update_per_episod = update_per_episod
-        self.old_episod = 0
-
-    def updateEpsilon(
-        self,
-        epsilon,
-        episode_count,
-        max_episodes=100,
-    ):
-        self.epsilon = epsilon
-        if self.update_per_episod:
-            if self.old_episod == episode_count:
-                return self.epsilon
-            else:
-                self.old_episod = episode_count
-        if self.policy == EpsilonPolicyType.DECAY:
-            return self.updateEpsilon_linear(episode_count, max_episodes)
-        elif self.policy == EpsilonPolicyType.SOFTLINEAR:
-            return self.updateEpsilon_SoftLinear(episode_count, max_episodes)
-        else:
-            return False
-
-    def updateEpsilon_linear(self, episode_count, max_episodes):
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
-        self.epsilon = min(self.epsilon, self.epsilon_min)
-        return self.epsilon
-
-    def updateEpsilon_SoftLinear(self, episode_count, max_episodes=200):
-        if self.epsilon > self.epsilon_min:
-            target_epsilon = max(
-                self.epsilon_min,
-                1.0 - (1.0 - self.epsilon_min) * (episode_count / max_episodes),
-            )
-            self.epsilon = self.epsilon * self.epsilon_decay + target_epsilon * (
-                1 - self.epsilon_decay
-            )
-        return self.epsilon
-
-
-class RewardHelper:
-    def __init__(
-        self,
-        progress_bonus: float = 0.05,
-        exploration_bonus: float = 0.1,
-        policy: RewardPolicyType = RewardPolicyType.ERM,
-    ):
-        self.progress_bonus = progress_bonus
-        self.exploration_bonus = progress_bonus
-        self.policy = policy
-        self.old_huristic = INFINIT
-        self.visited_states = {}
-
-    def findReward(self, state, reward, heuristic=None, lowerHuristicBetter=True):
-        if self.policy == RewardPolicyType.ERM:
-            return self._ERM(state, reward, heuristic, lowerHuristicBetter)
-        elif self.policy == RewardPolicyType.NONE:
-            return self._none(reward)
-
-    def _none(self, reward):
-        return reward
-
-    def _ERM(self, state, reward, heuristic=None, lowerHuristicBetter=True):
-        state_key = tuple(state.flatten()) if state is not None else None
-        is_new_state = state_key is not None and state_key not in self.visited_states
-        if is_new_state:
-            if is_new_state and state_key is not None:
-                self.visited_states[state_key] = (
-                    heuristic if heuristic is not None else INFINIT
-                )
-
-        progress = 0.0
-        if heuristic is not None and state_key is not None:
-            old_heuristic = self.visited_states[state_key]
-            if (
-                old_heuristic != INFINIT
-                and (heuristic < old_heuristic and lowerHuristicBetter)
-                or (heuristic > old_heuristic and not lowerHuristicBetter)
-            ):
-                progress = self.progress_bonus
-                self.visited_states[state_key] = heuristic
-
-        new_reward = reward
-        if is_new_state:
-            new_reward += self.exploration_bonus
-        if progress > 0:
-            new_reward += self.progress_bonus
-        return new_reward
